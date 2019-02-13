63dfda54de56e97776ed4735bd88ca11938804cf
SleepingFox <seanloganking@gmail.com>
2019-02-13 04:23:43 -0400

    quaternion_to_euler now returns radians

--- FbxToMimic.py
+++ FbxToMimic.py
@@ -12,16 +12,19 @@ def quaternion_to_euler(x, y, z, w):
     import math
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
-    X = math.degrees(math.atan2(t0, t1))
+    #X = math.degrees(math.atan2(t0, t1))
+    X = math.atan2(t0, t1)
 
     t2 = +2.0 * (w * y - z * x)
     t2 = +1.0 if t2 > +1.0 else t2
     t2 = -1.0 if t2 < -1.0 else t2
-    Y = math.degrees(math.asin(t2))
+    #Y = math.degrees(math.asin(t2))
+    Y = math.asin(t2)
 
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
-    Z = math.degrees(math.atan2(t3, t4))
+    #Z = math.degrees(math.atan2(t3, t4))
+    Z = math.atan2(t3, t4)
 
     return X, Y, Z
 

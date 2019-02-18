4d8a64ae95b959c071ff764bb0d72f88a537db42
SleepingFox <seanloganking@gmail.com>
2019-02-17 22:57:47 -0400

    Turn degrees to radians before Euler conversion

--- FbxToMimic.py
+++ FbxToMimic.py
@@ -31,7 +31,11 @@ def quaternion_to_euler(x, y, z, w):
 
     return X, Y, Z
 
-# euler_Original = (np.random.random(3) * 1).tolist() # Generate random rotation angles for XYZ within the range [0, 360)
+#euler_Original = (np.random.random(3) * 1).tolist() # Generate random rotation angles for XYZ within the range [0, 360)
+
+#euler_Original = [43.596, -69.205, 60.544]
+ 
+# euler_Original = [0.761, -1.208, 1.057]
 # quat = euler_to_quaternion(euler_Original[0], euler_Original[1], euler_Original[2]) # Convert to Quaternion
 # newEulerRot = quaternion_to_euler(quat[0], quat[1], quat[2], quat[3]) #Convert the Quaternion to Euler angles
 
@@ -54,13 +58,13 @@ with open('./rThigh.json') as json_data:
         print(f"[", file=output)
 
         #Start of Keyframes
-        numFrames = 30
+        numFrames = 900
         for i in range(0,numFrames):
             if i < (numFrames - 1):     # put comma at end of all arrays except last one (JSON format)
                 ending = ","
             else:
                 ending = ""
-            quat = euler_to_quaternion(d["X"][(2*i)+1], d["Y"][(2*i)+1], d["Z"][(2*i)+1])
+            quat = euler_to_quaternion(math.radians(d["X"][(2*i)+1]), math.radians(d["Y"][(2*i)+1]), math.radians(d["Z"][(2*i)+1]))
             #print(f"[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{quat[3]},{quat[0]},{quat[1]},{quat[2]},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]{ending}", file=output)
 
             print(f"[0.016666, 0.254329, 1.109693, 0.027481, -0.269159, 0.300407, 0.914427, 0.033652, 0.977585, 0.027517, 0.053675, -0.201714, 0.903554, 0.067610, 0.088587, -0.413728,{quat[3]},{quat[0]},{quat[1]},{quat[2]}, -1.704043, 0.959067, 0.068498, 0.123147, -0.245627, 0.807969, -0.554312, -0.084408, 0.181106, 0.787253, 0.829243, 0.189034, 0.167192, 0.498667, -0.662138, 0.964729, -0.229754, 0.084958, 0.096405, 0.513164, 0.645140, 0.008855, 0.566020, 1.537844]{ending}", file=output)

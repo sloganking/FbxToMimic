51dd435526e2208f64ca8fcd8e1a887d2dd026ea
SleepingFox <seanloganking@gmail.com>
2019-02-19 01:47:43 -0400

    Add files via upload

--- Utils/FbxToJson.py
+++ Utils/FbxToJson.py
@@ -1,6 +1,10 @@
 import numpy as np
 import math
 import json
+import re
+
+def isnumber(x):
+    return re.match("-?[0-9]+([.][0-9]+)?$", x) is not None
 
 def depthOf(str):
     depth = 0
@@ -27,35 +31,133 @@ with open("13_29.fbx") as input:
         #Start JSON object
         print(f"{{", file=output)
 
+        print(f"Content length: {len(content)}")
+        # Start of file conversion
         for x in range(0,len(content)):
             tokens = str.split(content[x])
             tabDepth = getTabs(content[x])
             strToPrint = ""
-
             curToken = 0
-            if len(tokens) > 0:
-                print(tokens[0])
-                if tokens[0][ len(tokens[0]) - 1] == ':':
-                    strToPrint += f"\"{tokens[0][:-1]}\": "
-                    curToken += 1
+            stringLeft = ""
 
             if len(tokens) > 0:
-                    if tokens[0][:1] != ";":    # if first char of first token = ";"
-                        for i in range(curToken,len(tokens)):
-                                    strToPrint += tokens[i]
+                if tokens[0][:1] == ";":    # if first char of first token = ";"
+                    nonANumber = 1
 
-                        # add comma if next depth == curDepth && not already have comma
+                elif content[x][ len(content[x]) - 2] == "{":   # if last char of line is "{"
+                    stringLeft = ""
+                    for i in range(curToken,len(tokens)):
+                        if tokens[i] != "{":
+                            stringLeft += tokens[i]
+                    stringLeft = stringLeft.replace("\"", "")
+                    strToPrint += f"\"{stringLeft}\": {{"
+                
+                elif tokens[0][ len(tokens[0]) - 1] == ':':     #elif first token is property
+                    
+                    # if len(tokens) < 2:
+                    #     strToPrint = "\"BadProperty\": \"none\""
+
+                    if tokens[0][:-1] == "CameraIndexName":
+                        strToPrint = "\"CameraIndexName\": \"bad property\""
+                    elif tokens[0][:-1] == "Shading":
+                        strToPrint = ""
+                    elif tokens[0][:-1] == "Month":
+                        strToPrint = "\"Month\": \"bad property\""
+                        #add comma if next depth == curDepth && not already have comma
                         if x < len(content) - 1:
                             if depthOf(content[x]) == depthOf(content[x + 1]):
-                                if content[x][ len(content[x]) - 2] != ',':     # -2 for last char that's not "\n"
+                                if strToPrint[len(strToPrint) - 1] != ",":
                                     strToPrint += ","
-            
+
+                    elif tokens[0][:-1] == "Key":
+                        strToPrint = "\"Key\": ["
+                    elif tokens[0][:-1] == "Color":
+                        print(f"{tabDepth}],", file=output)
+                        strToPrint = "\"Color\": 0"
+                    else:
+                        #add property to front
+                        strToPrint += f"\"{tokens[0][:-1]}\": "
+                        curToken += 1
+                        stringLeft = ""
+                        for i in range(curToken,len(tokens)):       #
+                            stringLeft += tokens[i]
+                        if len(stringLeft.split(",")) > 1:
+                            strToPrint += stringLeft.split(",")[1]
+                        else:
+                            strToPrint += stringLeft.split(",")[0]
+
+                        #add comma if next depth == curDepth && not already have comma
+                        if x < len(content) - 1:
+                            if depthOf(content[x]) == depthOf(content[x + 1]):
+                                if strToPrint[len(strToPrint) - 1] != ",":
+                                    strToPrint += ","
+
+                else:
+                    for i in range(curToken,len(tokens)):
+                        stringLeft += tokens[i]
+                    if len(stringLeft.split(",")) > 1:
+                        strToPrint += stringLeft.split(",")[1]
+                    else:
+                        strToPrint += stringLeft.split(",")[0]
+
+                    #add comma if next depth == curDepth && not already have comma
+                    if x < len(content) - 1:
+                        if depthOf(content[x]) == depthOf(content[x + 1]):
+                            if strToPrint[len(strToPrint) - 1] != ",":
+                                strToPrint += ","
+
+            #print(strToPrint)
+            # If something to print
             if strToPrint != "":
                 strToPrint = f"{tabDepth}{strToPrint}"
                 print(f"{strToPrint}", file=output)
-
 
         #End JSON object
         print(f"}}", file=output)
 
         print("Conversion completed...")
+
+#===============================================================================
+
+            # if content[x][ len(content[x]) - 2] == "{":
+            #     strToPrint += "{"
+
+            # elif len(tokens) > 0:
+            #         if tokens[0][:1] != ";":    # if first char of first token = ";"
+
+            #             stringLeft = ""
+            #             for i in range(curToken,len(tokens)):
+            #                 stringLeft += tokens[i]
+            #             print(stringLeft.split(","))
+
+
+
+            #             if len(stringLeft.split(",")) > 1:
+            #                 strToPrint += stringLeft.split(",")[1]
+            #             else:
+            #                 strToPrint += stringLeft.split(",")[0]
+
+            #             # add comma if next depth == curDepth && not already have comma
+            #             if x < len(content) - 1:
+            #                 if depthOf(content[x]) == depthOf(content[x + 1]):
+            #                     if strToPrint[len(strToPrint) - 1] != ",":
+            #                         strToPrint += ","
+
+
+            #                     # if content[x][ len(content[x]) - 2] != ',':     # -2 for last char that's not "\n"
+            #                     #     strToPrint += ","
+            
+
+
+
+#===============================================================================
+
+
+#if ;, print no line
+#elif the last char is "{"
+    #concatenate the entire line and make it a property name, 
+    # add "{ to the end of the line
+#elif first token is property
+    # create property name
+    # assign value (preference of second commad value)
+    # add comma if needed

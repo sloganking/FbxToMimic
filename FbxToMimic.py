6adb8b7dc821daf394a941c597e162cf5256eb43
SleepingFox <seanloganking@gmail.com>
2019-02-20 18:02:00 -0400

    Added posLocked
    if posLocked is true, fbx animation data for root rotation and position will not be translated
    
    Using this will allow developers to better debug the current issue with Euler to quaternion conversion

--- FbxToMimic.py
+++ FbxToMimic.py
@@ -10,26 +10,6 @@ def euler_to_quaternion(roll, pitch, yaw):
     qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
     return [qw, qx, qy, qz]
 
-def quaternion_to_euler(x, y, z, w):
-
-    #Calc X
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    X = math.atan2(t0, t1)
-
-    #Calc Y
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    Y = math.asin(t2)
-
-    #Calc Z
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    Z = math.atan2(t3, t4)
-
-    return X, Y, Z
-
 def getTimesIn(obj):
     timeList = []
 
@@ -97,25 +77,9 @@ def indexOfAnimated(anim):
     return index
 
 # Initilize variables
-Docs = [
-"duration of frame in seconds (1D)",
-"root position (3D)",
-"root rotation (4D)",
-"chest rotation (4D)",
-"neck rotation (4D)",
-"right hip rotation (4D)",
-"right knee rotation (1D)",
-"right ankle rotation (4D)",
-"right shoulder rotation (4D)",
-"right elbow rotation (1D)",
-"left hip rotation (4D)",
-"left knee rotation (1D)",
-"left ankle rotation (4D)",
-"left shoulder rotation (4D)",
-"left elbow rotation (1D)"
-]
 animated = ["Seconds", "Model:Model::hip", "Model:Model::hip", "Model:Model::chest","Model:Model::neck","Model:Model::rThigh","Model:Model::rShin","Model:Model::rFoot","Model:Model::rShldr","Model:Model::rForeArm","Model:Model::lThigh","Model:Model::lShin","Model:Model::lFoot","Model:Model::lShldr","Model:Model::lForeArm"]
 dimensions = [1,3,4,4,4,4,1,4,4,1,4,1,4,4,1]
+posLocked = True
 
 # Open files, Start of main program
 with open('./FbxJson.json') as json_data:
@@ -143,94 +107,105 @@ with open('./FbxJson.json') as json_data:
             else:
                 keyFrame.append((int(listOfTimes[i]) - int(listOfTimes[i-1])) * 0.00000000002)
 
-            # #Append Root position
-            xKey = d["Takes:"]["Take:19_15"]["Model:Model::hip"]["Channel:Transform"]["Channel:T"]["Channel:X"]["Key"]
-            yKey = d["Takes:"]["Take:19_15"]["Model:Model::hip"]["Channel:Transform"]["Channel:T"]["Channel:Y"]["Key"]
-            zKey = d["Takes:"]["Take:19_15"]["Model:Model::hip"]["Channel:Transform"]["Channel:T"]["Channel:Z"]["Key"]
 
-            #If Keys contain angle for desired time
-            if angleOfKeyAtTime(xKey,listOfTimes[i]) and angleOfKeyAtTime(yKey,listOfTimes[i]) and angleOfKeyAtTime(zKey,listOfTimes[i]):
+            if posLocked == False:
+                # #Append Root position
+                xKey = d["Takes:"]["Take:19_15"]["Model:Model::hip"]["Channel:Transform"]["Channel:T"]["Channel:X"]["Key"]
+                yKey = d["Takes:"]["Take:19_15"]["Model:Model::hip"]["Channel:Transform"]["Channel:T"]["Channel:Y"]["Key"]
+                zKey = d["Takes:"]["Take:19_15"]["Model:Model::hip"]["Channel:Transform"]["Channel:T"]["Channel:Z"]["Key"]
 
-                #Append angles
-                keyFrame.append(float(angleOfKeyAtTime(xKey,listOfTimes[i])))
-                keyFrame.append(float(angleOfKeyAtTime(yKey,listOfTimes[i])))
-                keyFrame.append(float(angleOfKeyAtTime(zKey,listOfTimes[i])))
+                #If Keys contain angle for desired time
+                if angleOfKeyAtTime(xKey,listOfTimes[i]) and angleOfKeyAtTime(yKey,listOfTimes[i]) and angleOfKeyAtTime(zKey,listOfTimes[i]):
 
-            # append old rotation
+                    #Append angles
+                    keyFrame.append(float(angleOfKeyAtTime(xKey,listOfTimes[i])))
+                    keyFrame.append(float(angleOfKeyAtTime(yKey,listOfTimes[i])))
+                    keyFrame.append(float(angleOfKeyAtTime(zKey,listOfTimes[i])))
+
+                # append old rotation
+                else:
+                    keyFrame.append(oldKeyframe[1])
+                    keyFrame.append(oldKeyframe[2])
+                    keyFrame.append(oldKeyframe[3])
+
+            #posLocked == true
             else:
-                keyFrame.append(oldKeyframe[1])
-                keyFrame.append(oldKeyframe[2])
-                keyFrame.append(oldKeyframe[3])
+                keyFrame.append(0)
+                keyFrame.append(2)
+                keyFrame.append(0)
 
             #Append 1D and 4D rotations
             for x in range(0,len(animated)):
                 if x > 1:
 
-                    #if angle is 4D
-                    if dimensions[x] == 4:
-                        xKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:X"]["Key"]
-                        yKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:Y"]["Key"]
-                        zKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:Z"]["Key"]
+                    #If posLocked, lock root rotation
+                    if posLocked and x == 2:
+                        keyFrame.append(1)
+                        keyFrame.append(0)
+                        keyFrame.append(0)
+                        keyFrame.append(0)
 
-                        if angleOfKeyAtTime(xKey,listOfTimes[i]) and angleOfKeyAtTime(yKey,listOfTimes[i]) and angleOfKeyAtTime(zKey,listOfTimes[i]):
-
-                            #Get angles
-                            X = float(angleOfKeyAtTime(xKey,listOfTimes[i]))
-                            Y = float(angleOfKeyAtTime(yKey,listOfTimes[i]))
-                            Z = float(angleOfKeyAtTime(zKey,listOfTimes[i]))
-
-                            #Calculate quaternion angle
-                            quat = euler_to_quaternion(math.radians(-Z), math.radians(-Y), math.radians(-X))
-
-                            #Append quaternion angle
-                            keyFrame.append(quat[0])
-                            keyFrame.append(quat[1])
-                            keyFrame.append(quat[2])
-                            keyFrame.append(quat[3])
-
-                        else:
-                            print(f"Missed rotation, Line:       {i + 5}")
-
-                            animIndex = indexOfAnimated(animated[x])
-
-                            for i in range(0,4):
-                                keyFrame.append(oldKeyframe[animIndex + i])
-
-                    #If angle is 1D (Knees and elbows)
-                    elif dimensions[x] == 1:
-
-                            #If Knees
-                            if animated[x] == "Model:Model::rShin" or animated[x] == "Model:Model::lShin":
-
-                                #if angle found, append
-                                xKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:X"]["Key"]
-                                if angleOfKeyAtTime(xKey,listOfTimes[i]):
-                                    X = math.radians(float(angleOfKeyAtTime(xKey,listOfTimes[i])))
-                                    keyFrame.append(X)
-
-                                #else append last angle
-                                else:
-                                    keyFrame.append(oldKeyframe[x])
-
-                            #If Elbows
-                            elif animated[x] == "Model:Model::rForeArm" or animated[x] == "Model:Model::lForeArm":
-
-                                #if angle found, append
-                                yKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:Y"]["Key"]
-                                if angleOfKeyAtTime(yKey,listOfTimes[i]):
-                                    Y = math.radians(float(angleOfKeyAtTime(xKey,listOfTimes[i])))
-                                    keyFrame.append(Y)
-
-                                #else append last angle
-                                else:
-                                    keyFrame.append(oldKeyframe[x])
                     else:
-                        print(f"Error on rotations Loop {x}")
+                        #if angle is 4D
+                        if dimensions[x] == 4:
+                            xKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:X"]["Key"]
+                            yKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:Y"]["Key"]
+                            zKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:Z"]["Key"]
 
+                            if angleOfKeyAtTime(xKey,listOfTimes[i]) and angleOfKeyAtTime(yKey,listOfTimes[i]) and angleOfKeyAtTime(zKey,listOfTimes[i]):
 
+                                #Get angles
+                                X = float(angleOfKeyAtTime(xKey,listOfTimes[i]))
+                                Y = float(angleOfKeyAtTime(yKey,listOfTimes[i]))
+                                Z = float(angleOfKeyAtTime(zKey,listOfTimes[i]))
 
+                                #Calculate quaternion angle
+                                quat = euler_to_quaternion(math.radians(-Z), math.radians(-Y), math.radians(-X))
 
+                                #Append quaternion angle
+                                keyFrame.append(quat[0])
+                                keyFrame.append(quat[1])
+                                keyFrame.append(quat[2])
+                                keyFrame.append(quat[3])
 
+                            else:
+                                print(f"Missed rotation, Line:       {i + 5}")
+
+                                animIndex = indexOfAnimated(animated[x])
+
+                                for i in range(0,4):
+                                    keyFrame.append(oldKeyframe[animIndex + i])
+
+                        #If angle is 1D (Knees and elbows)
+                        elif dimensions[x] == 1:
+
+                                #If Knees
+                                if animated[x] == "Model:Model::rShin" or animated[x] == "Model:Model::lShin":
+
+                                    #if angle found, append
+                                    xKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:X"]["Key"]
+                                    if angleOfKeyAtTime(xKey,listOfTimes[i]):
+                                        X = math.radians(float(angleOfKeyAtTime(xKey,listOfTimes[i])))
+                                        keyFrame.append(X)
+
+                                    #else append last angle
+                                    else:
+                                        keyFrame.append(oldKeyframe[x])
+
+                                #If Elbows
+                                elif animated[x] == "Model:Model::rForeArm" or animated[x] == "Model:Model::lForeArm":
+
+                                    #if angle found, append
+                                    yKey = d["Takes:"]["Take:19_15"][animated[x]]["Channel:Transform"]["Channel:R"]["Channel:Y"]["Key"]
+                                    if angleOfKeyAtTime(yKey,listOfTimes[i]):
+                                        Y = math.radians(float(angleOfKeyAtTime(xKey,listOfTimes[i])))
+                                        keyFrame.append(Y)
+
+                                    #else append last angle
+                                    else:
+                                        keyFrame.append(oldKeyframe[x])
+                        else:
+                            print(f"Error on rotations Loop {x}")
 
             #Turn keyFrame into a recordable JSON String
             keyFrameString = "["
